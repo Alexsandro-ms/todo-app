f31174d942ba7371bea4d1a64573a9b1e52eaeaf
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-06-25 17:33:51 -0300

    feat(index): configured part of application configuration

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+// Configuração da aplicação
+app.use(express.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+cors();
+
+module.exports = app;
