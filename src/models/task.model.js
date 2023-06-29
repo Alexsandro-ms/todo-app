fd8e71d17036f020414499ea9e2d4984f5b426c9
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-06-29 15:54:56 -0300

    feat(task.model): task model created

---
+++ src/models/task.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+   title: {
+    type: String,
+    required: true
+   },
+   description: {
+    type: String,
+    required: true
+   },
+   dueData: Date,
+   priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "low" 
+   },
+   completed: {
+      type: Boolean,
+      default: false
+   }
+});
+
+const TaskModel = mongoose.model("TaskModel", taskSchema);
+
+module.exports = TaskModel;
