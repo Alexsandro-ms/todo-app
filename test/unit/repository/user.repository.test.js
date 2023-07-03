47575d373453c740d4beb3e43a91cf75c7ed29b2
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-07-03 15:11:37 -0300

    test(user.repository): created the tests responsible for ensuring the user repository functionalities errors

--- test/unit/repository/user.repository.test.js
+++ test/unit/repository/user.repository.test.js
@@ -17,16 +17,33 @@ describe("User Repository", () => {
         password: "password"
       };
 
-      const createMock = jest.spyOn(UserRepository, 'create').mockResolvedValue(user);
+      const createMock = jest.spyOn(UserModel, 'create').mockResolvedValue(user);
 
       const createdUser = await UserRepository.create(user);
 
       expect(createMock).toHaveBeenCalledTimes(1);
-      expect(createdUser).toEqual(user);
+      expect(createdUser).toEqual(user);    
     } catch (e) {
       throw new Error(e);
     }
   });
+
+  test('Should return an error if one occurs while creating a user', async () => { 
+    const user = {
+        imagePath: "",
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@example.com",
+        password: "password",
+        task: []
+      };
+    
+      const mockError = new Error('An error occurred');
+      jest.spyOn(UserModel, 'create').mockRejectedValue(mockError);
+
+      const error = await UserRepository.create(user);
+      expect(error).toEqual(mockError);
+   });
 
   test('should remove a user', async () => { 
     try {
@@ -40,6 +57,14 @@ describe("User Repository", () => {
     } catch (e) {
       throw new Error(e);
     }
+  });
+
+  test('should return an error if one occurs while remove a user', async () => {
+    const mockError = new Error("An error occurred")
+    jest.spyOn(UserModel, 'findByIdAndDelete').mockRejectedValue(mockError)
+
+    const error = await UserRepository.remove(1)
+    expect(error).toEqual(mockError)
   });
 
   test('should edit a user', async () => { 
@@ -58,18 +83,69 @@ describe("User Repository", () => {
     }
   });
 
+  test('should return an erro if one occurs while update a user', async () => {
+    const user = {
+        id: "1",
+        tasks: [{title: "Lorem ipsum"}]
+      };
+
+      const mockError = new Error("An error occured")
+
+      jest.spyOn(UserModel, 'findByIdAndUpdate').mockRejectedValue(mockError)
+
+      const error = await UserRepository.edit(user.id, user.tasks)
+      expect(error).toEqual(mockError)
+  });
+
   test('should list a user', async () => { 
     try {
       const userId = '123456';
 
-      const findByIdMock = jest.spyOn(UserModel, 'findById').mockResolvedValue(user);
+      const findByIdMock = jest.spyOn(UserModel, 'findById').mockResolvedValue(userId);
 
       const foundUser = await UserRepository.list(userId);
 
       expect(findByIdMock).toHaveBeenCalledTimes(1);
-      expect(foundUser).toEqual(user);
+      expect(foundUser).toEqual(userId);
     } catch (e) {
       throw new Error(e);
     }
   });
+
+  test('should return the error if an error occurs while listing a user', async () => {
+    const mockError = new Error('An error occurred');
+    jest.spyOn(UserModel, 'findById').mockRejectedValue(mockError);
+
+    const error = await UserRepository.list("123456789");
+    expect(error).toEqual(mockError);
+  });
+
+  test('should find a user by email', async () => { 
+    try {
+        const user = {
+            email: 'johndoe@example.com'
+        }
+  
+        const findByIdMock = jest.spyOn(UserModel, 'findOne').mockResolvedValue(user);
+  
+        const foundUser = await UserRepository.find(user);
+  
+        expect(findByIdMock).toHaveBeenCalledTimes(1);
+        expect(foundUser).toEqual(user);
+      } catch (e) {
+        throw new Error(e);
+      }
+  });
+
+  test('should return the error if an error occurs while found a user', async () => { 
+    const user = {
+        email: 'johndoe@example.com'
+    }
+
+    const mockError = new Error('An error occurred');
+    jest.spyOn(UserModel, 'findOne').mockRejectedValue(mockError);
+
+    const error = await UserRepository.find(user.email);
+    expect(error).toEqual(mockError);
+   })
 });
