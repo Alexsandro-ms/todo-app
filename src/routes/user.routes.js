0fd6ca36d273f8fd893f92406db26c90dec26905
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-07-12 11:52:39 -0300

    feat(user.routes): created endpoint  which implements the functionality of searching for a user by id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const { createUser, removeUser, editUser } = require("../controllers/user.controller")
+const { createUser, removeUser, editUser, listUser } = require("../controllers/user.controller")
 
 const router = express.Router()
 
+router.get("/auth/list/:id", listUser)
 router.post("/auth/register", createUser)
 router.delete("/auth/remove/:id", removeUser)
 router.put("/auth/edit/:id", editUser)
