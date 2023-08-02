b73064d964175869b42e7f80e14fa09ab9ba8a5b
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-08-02 12:33:57 -0300

    feat(send-email): create sendToken text

--- src/utils/sendEmail/text.email.js
+++ src/utils/sendEmail/text.email.js
@@ -22,6 +22,36 @@ function generateEmailBody(userName) {
       </body>
       </html>
     `;
-  }
+}
+
+function sendToken(userName, token) {
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>Change your password</title>
+      <style>
+      </style>
+      </head>
+      <body>
+      <div class="container">
+      <img src="https://i.ibb.co/DtVPfjq/title.png" alt="title" border="0" style={{width: 100%}} />
+      <h2>Change your password</h2>
+      <p>Hi ${userName}, to change the password follow the steps below: </p>
+      <p>1. Click on the following link</p>
+      <p>2. Enter a new password<p>
+      <p>
+      <strong>The token will expire in 15 minutes</strong>
+      </p>
+      <p>TodoList Team</p>
+      <p>
+      <a href="https://localhost:5173/recover-password/${token}" class="btn">Change your password</a>
+      </p>
+      </div>
+      </body>
+      </html>
+    `;
+}
+
   
-  module.exports = generateEmailBody;
+  module.exports = { generateEmailBody, sendToken};
