af9df80885bf77ded5225ff8141d8e3ee38f9b41
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-08-03 13:25:50 -0300

    feat(generateRandomCode): create generate random code function

---
+++ src/utils/generateRandomCode/index.js
@@ -0,0 +1,16 @@
+const generateRandomCode = () => {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+  
+    let code = '';
+    for (let i = 0; i < 2; i++) {
+      code += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    for (let i = 0; i < 4; i++) {
+      code += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+  
+    return code;
+};
+
+module.exports = generateRandomCode
--- src/utils/validations/user.validation.js
+++ src/utils/validations/user.validation.js
@@ -16,7 +16,7 @@ let userUpdateSchema = yup.object({
   tasks: yup.array().of(yup.string()) 
 });
 
-const logInUserSchema = yup.object().shape({
+let logInUserSchema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().min(6).required(),
 });
