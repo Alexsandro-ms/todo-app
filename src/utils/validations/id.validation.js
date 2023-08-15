73be9a0297425e26b3a151d01dddcecab7819604
Alexsandro_Martins <alexsandromartins.santos@hotmail.com>
2023-08-15 16:31:55 -0300

    refactor(id.validation): refactor id validation

--- src/utils/validations/id.validation.js
+++ src/utils/validations/id.validation.js
@@ -1,7 +1,7 @@
 const yup = require("yup")
 
 let idSchema = yup.object({
-   id: yup.number("Id must be a number").required("Id is required").min(1)
+   id: yup.string("Id must be a string").required("Id is required").min(1)
 })
 
 module.exports = idSchema
